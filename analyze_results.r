eb157368d88f4ed942a14ad83be08f9ac431e624
grzegorz <gpietrusza@gmail.com>
2017-05-01 21:39:53 +0200

    tune params

--- analyze_results.r
+++ analyze_results.r
@@ -4,7 +4,7 @@ library(plotrix)
 data = npyLoad("/home/grzegorz/agh/prmiml/graphs/results.bin.npy")
 
 for (i in 1:4) {
-  matrix_data = matrix(data[i,], ncol=100, byrow=TRUE)
+  matrix_data = matrix(data[i,], ncol=1000, byrow=TRUE)
   color2D.matplot(matrix_data)
 }
 
