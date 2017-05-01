3d5797f4bc8a0f4d861ac41e24f893929d2146d3
grzegorz <gpietrusza@gmail.com>
2017-05-01 22:27:01 +0200

    add 3d plot

--- analyze_results.r
+++ analyze_results.r
@@ -1,5 +1,6 @@
 library(RcppCNPy)
 library(plotrix)
+library(scatterplot3d)
 
 data = npyLoad("/home/grzegorz/agh/prmiml/graphs/results.bin.npy")
 
@@ -10,3 +11,5 @@ for (i in 1:10) {
 
 pca  = prcomp(data, center = TRUE)
 plot(predict(pca))
+
+scatterplot3d(pca$x[,1], pca$x[,2], pca$x[,3], main = "PCA 3") 
