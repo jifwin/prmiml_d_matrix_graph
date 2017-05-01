fe71e8687bd144246cef8d80c5132550141cb1dc
grzegorz <gpietrusza@gmail.com>
2017-05-01 14:01:26 +0200

    generate more graphs

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+NUMBER_OF_GRAPHS_OF_EACH_TYPE = 15
 NUMBER_OF_NODES = 100
 PROBABILITY_FACTOR = 0.15
+
 
 
 def draw_graph(graph):
     nx.draw(graph)
     plt.show()
 
-#todo: different parameters?
 
-graph1 = nx.watts_strogatz_graph(NUMBER_OF_NODES, 2, PROBABILITY_FACTOR)
-draw_graph(graph1)
-graph2 = nx.erdos_renyi_graph(NUMBER_OF_NODES, PROBABILITY_FACTOR)
-draw_graph(graph2)
-graph3 = nx.barabasi_albert_graph(NUMBER_OF_NODES, 2)
-draw_graph(graph3)
-graph4 = nx.powerlaw_cluster_graph(NUMBER_OF_NODES, 2, PROBABILITY_FACTOR)
-draw_graph(graph4)
+#todo: different parameters?
+graphs = []
+
+for i in xrange(NUMBER_OF_GRAPHS_OF_EACH_TYPE):
+    graphs.append(nx.watts_strogatz_graph(NUMBER_OF_NODES, 2, PROBABILITY_FACTOR))
+    graphs.append(nx.erdos_renyi_graph(NUMBER_OF_NODES, PROBABILITY_FACTOR))
+    graphs.append(nx.barabasi_albert_graph(NUMBER_OF_NODES, 2))
+    graphs.append(nx.powerlaw_cluster_graph(NUMBER_OF_NODES, 2, PROBABILITY_FACTOR))
