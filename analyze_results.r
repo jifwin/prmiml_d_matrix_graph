00ea00e292d0394bb667859dfd8dca64db5c54cb
grzegorz <gpietrusza@gmail.com>
2017-05-01 21:39:46 +0200

    tune params

--- analyze_results.r
+++ analyze_results.r
@@ -1,4 +1,12 @@
 library(RcppCNPy)
-data = npyLoad("results.bin.npy")
+library(plotrix)
+
+data = npyLoad("/home/grzegorz/agh/prmiml/graphs/results.bin.npy")
+
+for (i in 1:4) {
+  matrix_data = matrix(data[i,], ncol=100, byrow=TRUE)
+  color2D.matplot(matrix_data)
+}
+
 pca  = prcomp(data, center = TRUE)
 plot(predict(pca))
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import networkx as nx
 import numpy as np
 from networkx import shortest_path
 
-NUMBER_OF_GRAPHS_OF_EACH_TYPE = 15
+NUMBER_OF_GRAPHS_OF_EACH_TYPE = 10
 NUMBER_OF_NODES = 1000
 PROBABILITY_FACTORS = np.linspace(0.05,0.1,3)
 M_OR_K_PARAMS = [4,5]
@@ -17,17 +17,18 @@ for i in xrange(NUMBER_OF_GRAPHS_OF_EACH_TYPE):
     graphs.append(nx.barabasi_albert_graph(NUMBER_OF_NODES, random.choice(M_OR_K_PARAMS)))
     graphs.append(nx.powerlaw_cluster_graph(NUMBER_OF_NODES, random.choice(M_OR_K_PARAMS), random.choice(PROBABILITY_FACTORS)))
 
+max_diameter = max([nx.diameter(graph) for graph in graphs])
 
 def calculate_b_matrix(graph):
     print graph
-    b_matrix = np.zeros((NUMBER_OF_NODES, NUMBER_OF_NODES))
+    b_matrix = np.zeros((max_diameter, NUMBER_OF_NODES))
 
     for source_node in graph.nodes():
         path = shortest_path(graph, source_node)
         path.pop(source_node)
         distances = {key: len(values) for (key, values) in path.iteritems()}
         for (l, k) in distances.iteritems():
-            b_matrix[k, l] += 1
+            b_matrix[k-2, l] += 1
 
     return b_matrix.flatten()
 
