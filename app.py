d0571e807b5a7b9dce1cc31ca285b6391bdd0b1b
grzegorz <gpietrusza@gmail.com>
2017-05-01 18:06:18 +0200

    run for all graphs

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import networkx as nx
+import numpy as np
+import collections
+
+from networkx import shortest_path
 import matplotlib.pyplot as plt
 
-NUMBER_OF_GRAPHS_OF_EACH_TYPE = 15
-NUMBER_OF_NODES = 100
+NUMBER_OF_GRAPHS_OF_EACH_TYPE = 50
+NUMBER_OF_NODES = 1000
 PROBABILITY_FACTOR = 0.15
 
 
@@ -16,7 +20,24 @@ def draw_graph(graph):
 graphs = []
 
 for i in xrange(NUMBER_OF_GRAPHS_OF_EACH_TYPE):
-    graphs.append(nx.watts_strogatz_graph(NUMBER_OF_NODES, 2, PROBABILITY_FACTOR))
+    graphs.append(nx.connected_watts_strogatz_graph(NUMBER_OF_NODES, 2, PROBABILITY_FACTOR))
     graphs.append(nx.erdos_renyi_graph(NUMBER_OF_NODES, PROBABILITY_FACTOR))
     graphs.append(nx.barabasi_albert_graph(NUMBER_OF_NODES, 2))
     graphs.append(nx.powerlaw_cluster_graph(NUMBER_OF_NODES, 2, PROBABILITY_FACTOR))
+
+b_matrices = []
+i = 0
+for graph in graphs:
+    print i #todo: just for debug
+    nodes = graph.nodes()
+    b_matrix = np.zeros((NUMBER_OF_NODES, NUMBER_OF_NODES))
+
+    for source_node in graph.nodes():
+        path = shortest_path(graph, source_node)
+        path.pop(source_node)
+        distances = [len(path[key]) for key in path.keys()]
+        for (l, k) in collections.Counter(distances).iteritems():
+            b_matrix[k,l] += 1
+
+    b_matrices.append(b_matrix)
+    i += 1
