3b342a53a4efafbed98ef589d744116f309bc131
grzegorz <gpietrusza@gmail.com>
2017-05-01 21:47:26 +0200

    plot for ten first graphs

--- analyze_results.r
+++ analyze_results.r
@@ -3,7 +3,7 @@ library(plotrix)
 
 data = npyLoad("/home/grzegorz/agh/prmiml/graphs/results.bin.npy")
 
-for (i in 1:4) {
+for (i in 1:10) {
   matrix_data = matrix(data[i,], ncol=1000, byrow=TRUE)
   color2D.matplot(matrix_data)
 }
