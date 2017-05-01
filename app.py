bac3d3961f773a17dcf4520f687efa2e2df7025e
grzegorz <gpietrusza@gmail.com>
2017-05-01 20:51:29 +0200

    random parameters for graphs

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
+import random
 from joblib import Parallel, delayed
 import networkx as nx
 import numpy as np
-
 from networkx import shortest_path
 
 NUMBER_OF_GRAPHS_OF_EACH_TYPE = 12
 NUMBER_OF_NODES = 1000
-PROBABILITY_FACTOR = 0.15
+PROBABILITY_FACTORS = np.linspace(0.1,0.5,5)
+M_OR_K_PARAMS = [2,3,4,5]
 
-# todo: different parameters?
 graphs = []
 
 for i in xrange(NUMBER_OF_GRAPHS_OF_EACH_TYPE):
-    graphs.append(nx.connected_watts_strogatz_graph(NUMBER_OF_NODES, 2, PROBABILITY_FACTOR))
-    graphs.append(nx.erdos_renyi_graph(NUMBER_OF_NODES, PROBABILITY_FACTOR))
-    graphs.append(nx.barabasi_albert_graph(NUMBER_OF_NODES, 2))
-    graphs.append(nx.powerlaw_cluster_graph(NUMBER_OF_NODES, 2, PROBABILITY_FACTOR))
+    graphs.append(nx.connected_watts_strogatz_graph(NUMBER_OF_NODES, random.choice(M_OR_K_PARAMS), random.choice(PROBABILITY_FACTORS)))
+    graphs.append(nx.erdos_renyi_graph(NUMBER_OF_NODES, random.choice(PROBABILITY_FACTORS)))
+    graphs.append(nx.barabasi_albert_graph(NUMBER_OF_NODES, random.choice(M_OR_K_PARAMS)))
+    graphs.append(nx.powerlaw_cluster_graph(NUMBER_OF_NODES, random.choice(M_OR_K_PARAMS), random.choice(PROBABILITY_FACTORS)))
 
 
 def calculate_b_matrix(graph):
